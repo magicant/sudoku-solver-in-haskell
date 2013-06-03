eda0aba7922bd9960b9208b7539a00464b61ccb3
WATANABE Yuki <magicant@wonderwand.net>
2013-06-03 23:25:53 +0900

    Redefine blockBounds using blockNumBounds

--- Board.hs
+++ Board.hs
@@ -15,6 +15,9 @@ col = snd
 numBounds :: (Int, Int)
 numBounds = (1, 9)
 
+blockNumBounds :: [(Int, Int)]
+blockNumBounds = [(1, 3), (4, 6), (7, 9)]
+
 rowBounds :: Int -> (Index, Index)
 rowBounds r = ((r, fst numBounds), (r, snd numBounds))
 
@@ -24,11 +27,10 @@ colBounds c = ((fst numBounds, c), (snd numBounds, c))
 -- | Bounds of the block containing the argument index.
 blockBounds :: Index -> (Index, Index)
 blockBounds (r, c) = ((rl, cl), (ru, cu))
-  where (rl, ru) = bounds r
-        (cl, cu) = bounds c
-        bounds n | 1 <= n && n <= 3 = (1, 3)
-                 | 4 <= n && n <= 6 = (4, 6)
-                 | 7 <= n && n <= 9 = (7, 9)
+  where
+    (rl, ru) = boundsContaining r
+    (cl, cu) = boundsContaining c
+    boundsContaining n = head $ filter (flip inRange n) blockNumBounds
 
 indexBounds :: (Index, Index)
 indexBounds = ((l, l), (u, u))
